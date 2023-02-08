84bba005208d661e7709888eeff6be5fc594e682
MaroonShaded <51722734+MaroonShaded@users.noreply.github.com>
2023-02-08 22:58:41 +0000

    Remove tooltip

--- src/main/java/maroonshaded/gildedarmor/item/GildedArmorItem.java
+++ src/main/java/maroonshaded/gildedarmor/item/GildedArmorItem.java
@@ -1,29 +1,13 @@
 package maroonshaded.gildedarmor.item;
 
-import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Formatting;
-import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 public class GildedArmorItem extends ArmorItem
 {
     public GildedArmorItem(ArmorMaterial material, EquipmentSlot slot, Settings settings)
     {
         super(material, slot, settings);
-    }
-
-    @Override
-    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context)
-    {
-        for (int i = 1; i <= 3; i++)
-            tooltip.add(new TranslatableText("item.gildedarmor.generic.upgrading_note" + i).formatted(Formatting.GRAY));
     }
 }
