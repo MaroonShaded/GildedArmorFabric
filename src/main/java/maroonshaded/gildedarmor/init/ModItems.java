09428e2554cfb6494937b7cfbbd991bfacba0a27
MaroonShaded <51722734+MaroonShaded@users.noreply.github.com>
2023-07-25 19:32:51 +0100

    Fix slots for gilded enderite

--- src/main/java/maroonshaded/gildedarmor/init/ModItems.java
+++ src/main/java/maroonshaded/gildedarmor/init/ModItems.java
@@ -23,8 +23,8 @@ public class ModItems
 
     // For the Enderite mod
     public static final Item GILDED_ENDERITE_HELMET = addItem("gilded_enderite_helmet", new GildedEnderiteHelmetItem(new Item.Settings().fireproof()));
-    public static final Item GILDED_ENDERITE_CHESTPLATE = addItem("gilded_enderite_chestplate", new GildedArmorItem(ModArmorMaterial.GILDED_ENDERITE, ArmorItem.Type.HELMET, new Item.Settings().fireproof()));
-    public static final Item GILDED_ENDERITE_LEGGINGS = addItem("gilded_enderite_leggings", new GildedArmorItem(ModArmorMaterial.GILDED_ENDERITE, ArmorItem.Type.CHESTPLATE, new Item.Settings().fireproof()));
+    public static final Item GILDED_ENDERITE_CHESTPLATE = addItem("gilded_enderite_chestplate", new GildedArmorItem(ModArmorMaterial.GILDED_ENDERITE, ArmorItem.Type.CHESTPLATE, new Item.Settings().fireproof()));
+    public static final Item GILDED_ENDERITE_LEGGINGS = addItem("gilded_enderite_leggings", new GildedArmorItem(ModArmorMaterial.GILDED_ENDERITE, ArmorItem.Type.LEGGINGS, new Item.Settings().fireproof()));
     public static final Item GILDED_ENDERITE_BOOTS = addItem("gilded_enderite_boots", new GildedArmorItem(ModArmorMaterial.GILDED_ENDERITE, ArmorItem.Type.BOOTS, new Item.Settings().fireproof()));
 
     protected static Item addItem(String id, Item item)
